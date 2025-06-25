7405be5dad839ee306c1b31b1233c41a81a444ed
Romeo Lanzino <romeo.lanzino@gmail.com>
2025-06-25 12:44:43 +0200

    updated view results

--- do_final_results.sh
+++ do_final_results.sh
@@ -1,4 +1,6 @@
 # python do_results.py --use_horizontal_landmarks --use_vertical_landmarks
 # python do_results.py --use_horizontal_landmarks --use_vertical_landmarks --normalize_landmarks
-python do_results.py --use_horizontal_landmarks --use_vertical_landmarks --use_horizontal_image --use_vertical_image
-python do_results.py --use_horizontal_landmarks --use_vertical_landmarks --use_horizontal_image --use_vertical_image --normalize_landmarks
+# python do_results.py --use_horizontal_landmarks --use_vertical_landmarks --use_horizontal_image --use_vertical_image
+# python do_results.py --use_horizontal_landmarks --use_vertical_landmarks --use_horizontal_image --use_vertical_image --normalize_landmarks
+python do_results.py --use_horizontal_landmarks --use_horizontal_image --normalize_landmarks
+python do_results.py --use_vertical_landmarks --use_vertical_image --normalize_landmarks
